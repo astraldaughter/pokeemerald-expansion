2cd1eb1d52d6edda2309988e84430cb7b3346b62
astraldaughter <108581043+astraldaughter@users.noreply.github.com>
2024-09-26 12:30:17 +0100

    Battle UI changes

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -2399,7 +2399,10 @@ static void UpdateStatusIconInHealthbox(u8 healthboxSpriteId)
     }
     else
     {
-        statusGfxPtr = GetHealthboxElementGfxPtr(HEALTHBOX_GFX_39);
+        if (IsDoubleBattle())
+            statusGfxPtr = GetHealthboxElementGfxPtr(HEALTHBOX_GFX_40);
+        else
+            statusGfxPtr = GetHealthboxElementGfxPtr(HEALTHBOX_GFX_39);
 
         for (i = 0; i < 3; i++)
             CpuCopy32(statusGfxPtr, (void *)(OBJ_VRAM0 + (gSprites[healthboxSpriteId].oam.tileNum + tileNumAdder + i) * TILE_SIZE_4BPP), 32);
