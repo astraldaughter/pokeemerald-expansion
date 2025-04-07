8820f1c11f75d284f9472d2423ddb797d69a9d0e
astraldaughter <astraldaughter@gmail.com>
2025-04-07 23:56:23 +0100

    Config changes

--- include/bw_summary_screen.h
+++ include/bw_summary_screen.h
@@ -7,7 +7,7 @@
 #define BW_SUMMARY_SCREEN                           TRUE
 
 // constants
-#define BW_MAX_MOVE_DESCRIPTION_LENGTH              60      // this only needs to be updated if you use auto-formatting
+#define BW_MAX_MOVE_DESCRIPTION_LENGTH              100     // this only needs to be updated if you use auto-formatting
 #define BW_IV_EV_HIDDEN                             0       // don't show IVs and EVs
 #define BW_IV_EV_GRADED                             1       // show graded values for IVs and EVs
 #define BW_IV_EV_PRECISE                            2       // show precise values for IVs and EVs
@@ -16,22 +16,22 @@
 
 // configs
 #define BW_SUMMARY_AUTO_FORMAT_MOVE_DESCRIPTIONS    TRUE                // automatically formats move descriptions to fit the new box size. disable if you want to format them manually
-#define BW_SUMMARY_NATURE_COLORS                    TRUE                // color stats increased or reduced by nature, red = boosted, blue = reduced
+#define BW_SUMMARY_NATURE_COLORS                    FALSE                // color stats increased or reduced by nature, red = boosted, blue = reduced
 #define BW_SUMMARY_NATURE_ARROWS                    FALSE               // arrows to show stats increased or reduced by nature
 #define BW_SUMMARY_CATEGORY_ICONS                   TRUE                // determines whether category (split) icons are shown or not
 #define BW_SUMMARY_IV_EV_DISPLAY                    BW_IV_EV_HIDDEN     // determines how to show IVs and EVs
 #define BW_SUMMARY_DECAP                            FALSE               // indicates if summary screen-specific strings should be decapitalized
 #define BW_SUMMARY_SHOW_FRIENDSHIP                  FALSE               // show a heart that fills up to indicate friendship value
-#define BW_SUMMARY_BW_STATUS_ICONS                  TRUE                // use Gen 5 style status icons instead of the default ones.
-#define BW_SUMMARY_BW_TYPE_ICONS                    TRUE                // use Gen 5 style type icons instead of the default ones.
+#define BW_SUMMARY_BW_STATUS_ICONS                  FALSE               // use Gen 5 style status icons instead of the default ones.
+#define BW_SUMMARY_BW_TYPE_ICONS                    FALSE                // use Gen 5 style type icons instead of the default ones.
                                                                         // out of the box the vanilla icons don't fit well, this is mostly a compatibility
                                                                         // option for people who already use custom icons everywhere else
-#define BW_SUMMARY_SCROLLING_BG                     TRUE                // enables scrolling animated background
-#define BW_SUMMARY_BG_BLEND                         TRUE                // enables alpha blending for the main UI (semi-transparency)
-#define BW_SUMMARY_MON_IDLE_ANIMS                   TRUE                // loops the mon animations regularly as an "idle" anim
-#define BW_SUMMARY_MON_SHADOWS                      TRUE                // displays a shadow for the mon sprite
+#define BW_SUMMARY_SCROLLING_BG                     FALSE               // enables scrolling animated background
+#define BW_SUMMARY_BG_BLEND                         FALSE               // enables alpha blending for the main UI (semi-transparency)
+#define BW_SUMMARY_MON_IDLE_ANIMS                   FALSE               // loops the mon animations regularly as an "idle" anim
+#define BW_SUMMARY_MON_SHADOWS                      FALSE               // displays a shadow for the mon sprite
 #define BW_SUMMARY_SHOW_TERA_TYPE                   FALSE               // show tera type icons
-#define BW_SUMMARY_SHOW_CONTEST_MOVES               TRUE                // show the contest moves page
+#define BW_SUMMARY_SHOW_CONTEST_MOVES               FALSE                // show the contest moves page
 
 // Looking for configs for renaming mons and relearning moves? Those use the standard expansion configs
 // P_SUMMARY_SCREEN_RENAME and P_SUMMARY_SCREEN_MOVE_RELEARNER in include/config/pokemon.h
